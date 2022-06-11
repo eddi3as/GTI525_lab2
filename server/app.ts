76cc42717417955ad30ea4127471427f5de9b24a
Eddi3_As <AQ43820@etsmtl.net>
2022-06-11 15:10:12 -0400

    SSR for proj

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 import { compteurRoutes } from './routes/compteurRouter';
 import { fontaineRoutes } from './routes/fontaineRouter';
 import { statsRoutes } from './routes/statsRouter';
 
+const path = __dirname + '/views/';
+const corsOptions = {
+  origin: "http://localhost:3001"//maybe 3000 || 3001 for the API
+};
 // Creates and configures an ExpressJS web server.
 class App {
   public expressApp: express.Application;
@@ -13,19 +18,21 @@ class App {
     this.expressApp = express();
     this.middleware();
     this.routes();
+    this.expressApp.use(express.static(path) as express.RequestHandler);
+    this.expressApp.use(cors(corsOptions));
   }
 
   private middleware(): void {
     this.expressApp.use(logger('dev') as express.RequestHandler);
     this.expressApp.use(express.json() as express.RequestHandler);
-    this.expressApp.use(express.urlencoded({ extended: false }) as express.RequestHandler);
+    this.expressApp.use(express.urlencoded({ extended: true }) as express.RequestHandler);
   }
 
   // Configure API endpoints.
   private routes(): void {
     let router = express.Router();
     router.get('/', (req, res, next) => {
-      res.send("GTI525 server running!");
+      res.sendFile(path + "index.html");
     });
 
     this.expressApp.use('/', router);  // routage de base
