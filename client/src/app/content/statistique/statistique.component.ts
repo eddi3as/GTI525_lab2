1f3687e1100d002a1f25325871a83266b62d9b4d
Eddi3_As <AQ43820@etsmtl.net>
2022-06-13 19:33:30 -0400

    stats refactoring

--- client/src/app/content/statistique/statistique.component.ts
+++ client/src/app/content/statistique/statistique.component.ts
@@ -24,7 +24,15 @@ export class StatistiqueComponent implements OnInit {
       fin: this.dateTo
     }
     localStorage.setItem("stats", JSON.stringify(info));
-    this.router.navigateByUrl("/chart");
+    //CALL API FROM THIS MODULE & PASS ARRAY TO CHART
+    /*
+    this.router.navigate(['/chart'], {
+      state: {
+        frontEnd: JSON.stringify({ framwork: 'Angular', version: '9' }),
+        site: 'edupala.com'
+      } 
+    });
+    */
   }
 
 }
