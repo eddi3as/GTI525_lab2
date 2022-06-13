2ee7dfef2abeb96d924401c7b89e0a5b4ef02d6f
Eddi3_As <AQ43820@etsmtl.net>
2022-06-13 18:20:08 -0400

    dev env refactoring

--- server/app.ts
+++ server/app.ts
@@ -12,25 +12,24 @@ const corsOptions = {
 // Creates and configures an ExpressJS web server.
 class App {
   public expressApp: express.Application;
-  private BASE_API = '/gti525/v1';
+  private BASE_API =  '/gti525/v1';
 
   constructor() {
     this.expressApp = express();
     this.middleware();
     this.routes();
-    if(this.isInProd()){
-      this.expressApp.use(express.static(path) as express.RequestHandler);
-      this.expressApp.use(cors(corsOptions));
-    }else{
-      
-      this.expressApp.use(cors());
-    }
   }
 
   private middleware(): void {
     this.expressApp.use(logger('dev') as express.RequestHandler);
     this.expressApp.use(express.json() as express.RequestHandler);
     this.expressApp.use(express.urlencoded({ extended: true }) as express.RequestHandler);
+    this.expressApp.use(express.static(path) as express.RequestHandler);
+    if(this.isInProd()){
+      this.expressApp.use(cors(corsOptions));
+    }else{
+      this.expressApp.use(cors());
+    }
   }
 
   private isInProd(){
@@ -41,11 +40,7 @@ class App {
   private routes(): void {
     let router = express.Router();
     router.get('/', (req, res, next) => {
-      if(this.isInProd()){
         res.sendFile(path + "index.html");
-      }else{
-        res.send("GTI525 server running!")
-      }
     });
 
     this.expressApp.use('/', router);  // routage de base
