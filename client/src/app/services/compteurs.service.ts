9e61065910d4a418ea90e5a4c5caa1c94c0f69e1
Vincent Chang <vincchang08@hotmail.com>
2022-06-26 17:15:51 -0400

    added map markers

--- client/src/app/services/compteurs.service.ts
+++ client/src/app/services/compteurs.service.ts
@@ -1,16 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Compteur } from 'src/app/models/compteur';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompteurService {
   private url = environment.apiKey + '/compteurs';
-  
+  allCompteursSubject = new Subject<any>();
+  selectedCompteurSubject = new Subject<any>();
+
   constructor(private http: HttpClient) { }
 
   getCompteurs(){
     return this.http.get(this.url);
   }
+
+  setAllCompteurs(allCompteurs: Compteur[]) {
+    this.allCompteursSubject.next(allCompteurs);
+  }
+  setSelectedCompteur(selectedCompteur: Compteur) {
+    this.selectedCompteurSubject.next(selectedCompteur);
+  }
+  getAllCompteurs(): Observable<any> {
+    return this.allCompteursSubject.asObservable();
+  }
+  getSelectedCompteur(): Observable<any> {
+    return this.selectedCompteurSubject.asObservable();
+  }
+
 }
