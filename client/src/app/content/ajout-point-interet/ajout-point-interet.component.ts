d3d4b0df7109db7c6b4fe0e7965d680892a9f648
Maxime Chaussé <maxime.chausse.1@ens.etsmtl.ca>
2022-07-17 13:01:15 -0400

    Completed the form fields

--- client/src/app/content/ajout-point-interet/ajout-point-interet.component.ts
+++ client/src/app/content/ajout-point-interet/ajout-point-interet.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ajout-point-interet',
-  templateUrl: './ajout-point-interet.component.html',
-  styleUrls: ['./ajout-point-interet.component.css']
+    selector: 'app-ajout-point-interet',
+    templateUrl: './ajout-point-interet.component.html',
+    styleUrls: ['./ajout-point-interet.component.css']
 })
 export class AjoutPointInteretComponent implements OnInit {
+    public types: string[] = ["Fontaines à boire", "Atelier de vélo"]
+    public selectedType: string = ""
+    public annees: number[] = []
+    public selectedAnnee: number = new Date().getFullYear()
+    public nomLieu: string = ""
+    public adresse: string = ""
+    public arrondissement: string = ""
+    public longitude: number = 0
+    public latitude: number = 0
+    public remarque: string = ""
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        
+        for(let i = 0; i <= 100; i++) 
+            this.annees.push(this.selectedAnnee - i)
+    }
 
 }
